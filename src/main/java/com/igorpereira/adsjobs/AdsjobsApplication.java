6069e805192e1f2e8bf90201cc2e46666448e617
IgorPLima <igorpereira201425@gmail.com>
2021-06-13 17:59:59 -0300

    Fist commit project structure create

---
+++ src/main/java/com/igorpereira/adsjobs/AdsjobsApplication.java
@@ -0,0 +1,13 @@
+package com.igorpereira.adsjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorpereira/adsjobs/AdsjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorpereira.adsjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
